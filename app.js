ccd2f26f768a71f4f704709f29a69f7b1c0263c9
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.com>
2023-06-23 19:41:25 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+    return (
+        <div style={{ backgroundColor: '#181818', minHeight: "100vh", color: "#fff" }}>
+            <h1 style={{ textAlign: 'center' }}>Hello Github Actions!!!</h1>
+        </div>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
