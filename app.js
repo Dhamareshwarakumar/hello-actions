c4b86ecf46984bec74b7d7a8bf8c4645d29b3456
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.com>
2023-06-24 14:22:33 +0530

    test13

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const App = () => {
         <div style={{ backgroundColor: '#181818', minHeight: "100vh", color: "#fff" }}>
             <h1 style={{ textAlign: 'center' }}>Hello Github Actions!!!</h1>
             <p>Changes from test branch....</p>
+            <p>Testing Branch rules</p>
         </div>
     );
 };
